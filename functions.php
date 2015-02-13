316199058286c59ecf5b53b3814525003da8d88a
Jenna Murphy <jmurphy@mcad.edu>
2015-02-12 23:16:47 -0600

    updated functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,1 @@
-<?php register_nav_menus();register_sidebar(array(	'before_widget' => '<div class="sidebar">',	'after_widget' => '</div>',	'before_title' => 'div class="title">',	'after_title' => '</div>',	));
+<?php register_nav_menus();register_sidebar(array(	'before_widget' => '<div class="sidebar">',	'after_widget' => '</div>',	'before_title' => 'div class="title">',	'after_title' => '</div>',	));?>
